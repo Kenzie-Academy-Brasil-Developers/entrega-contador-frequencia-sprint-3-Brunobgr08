566de56252ce4de672a7b580c4598f457698eda1
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2021-07-15 16:00:21 -0300

    Resolvendo Contador de Frequência

---
+++ script/script.js
@@ -0,0 +1,80 @@
+
+
+const letterCounts = {};
+const wordsCounts = {};
+let typedText;
+
+const button = document.getElementById("countButton");
+button.addEventListener("click", mostrarNaTela );
+
+function carregarTexto(){
+
+   typedText = document.getElementById("textInput").value;
+
+   typedText = typedText.toLowerCase(); 
+   typedText = typedText.replace(/[^a-z'\s]+/g, ""); 
+}
+
+function letterCount(){
+
+   let typedTextNonSpace = typedText.replace(/\s+/g, '');
+   
+   for (let i = 0; i < typedTextNonSpace.length; i++) {
+
+      let currentLetter = typedTextNonSpace[i];
+      
+      if (letterCounts[currentLetter] === undefined) {
+         letterCounts[currentLetter] = 1; 
+      } else { 
+         letterCounts[currentLetter]++; 
+      }
+   }
+}
+
+function wordCount(){
+
+   let typedWords = typedText.split(" ");
+
+   for (let i = 0; i < typedWords.length; i++){
+
+      let currentWord = typedWords[i];
+
+      if (wordsCounts[currentWord] === undefined){
+         wordsCounts[currentWord] = 1;
+      } else {
+         wordsCounts[currentWord]++;
+      }
+   }
+}
+
+
+function mostrarNaTela(){
+
+   
+   carregarTexto();
+
+   letterCount();
+   wordCount();
+
+   for (let letter in letterCounts) { 
+      const span = document.createElement("span");
+      span.className = "span__containerContador"; 
+      const textContent = `"${letter}": ${letterCounts[letter]}`;
+      span.innerText = textContent; 
+
+      const letters = document.getElementById("lettersDiv");
+      letters.style.height = ((Object.keys(letterCounts).length * 20) / 2 ) + "px";
+      letters.appendChild(span); 
+   }
+
+   for ( let word in wordsCounts){
+      const spanWord = document.createElement("span");
+      spanWord.className = "span__containerContador";
+      const textContentWord = `"${word}": ${wordsCounts[word]}`;
+      spanWord.innerText = textContentWord;
+
+      const words = document.getElementById("wordsDiv");
+      words.style.height = ((Object.keys(wordsCounts).length * 20) / 2 ) + "px";
+      words.appendChild(spanWord);
+   }
+}
