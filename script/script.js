61a8d7ad74cc60dea0401bbe1e33d8227a111070
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2021-09-07 22:48:40 -0300

    Refatorando Contador de Frequencia

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@
 let letterCounts = {};
 let wordsCounts = {};
 let typedText;
+const msg = document.getElementById('msgToUser');
 
 const button = document.getElementById("countButton");
 button.addEventListener("click", mostrarNaTela );
@@ -11,8 +12,8 @@ function carregarTexto(){
 
    typedText = document.getElementById("textInput").value;
 
-   typedText = typedText.toLowerCase(); 
-   typedText = typedText.replace(/[^a-z'\s]+/g, ""); 
+   typedText = typedText.toLowerCase();
+   typedText = typedText.replace(/[^a-z'\s]+/g, "");
 }
 
 function letterCount(){
@@ -20,16 +21,21 @@ function letterCount(){
    letterCounts = {};
 
    let typedTextNonSpace = typedText.replace(/\s+/g, '');
-   
-   for (let i = 0; i < typedTextNonSpace.length; i++) {
 
-      let currentLetter = typedTextNonSpace[i];
-      
-      if (letterCounts[currentLetter] === undefined) {
-         letterCounts[currentLetter] = 1; 
-      } else { 
-         letterCounts[currentLetter]++; 
+   if (typedTextNonSpace !== ""){
+
+      for (let i = 0; i < typedTextNonSpace.length; i++) {
+
+         let currentLetter = typedTextNonSpace[i];
+
+         if (letterCounts[currentLetter] === undefined) {
+            letterCounts[currentLetter] = 1;
+         } else {
+            letterCounts[currentLetter]++;
+         }
       }
+   } else {
+      msgToUser();
    }
 }
 
@@ -39,19 +45,27 @@ function wordCount(){
 
    let typedWords = typedText.split(" ");
 
-   for (let i = 0; i < typedWords.length; i++){
+   if (typedWords.length !== 0){
 
-      let currentWord = typedWords[i];
+      for (let i = 0; i < typedWords.length; i++){
 
-      if (wordsCounts[currentWord] === undefined){
-         wordsCounts[currentWord] = 1;
-      } else {
-         wordsCounts[currentWord]++;
+         let currentWord = typedWords[i];
+
+         if (wordsCounts[currentWord] === undefined){
+            wordsCounts[currentWord] = 1;
+         } else {
+            wordsCounts[currentWord]++;
+         }
       }
+
+   } else {
+      msgToUser();
    }
 }
 
 function limparAll(){
+
+   msg.innerText = '';
 
    const letras = document.getElementById("lettersDiv");
    while (letras.firstChild){
@@ -65,36 +79,45 @@ function limparAll(){
 
 }
 
+function msgToUser(){
+      msg.innerText = "Insira um texto que contenha letras e palavras!";
+}
+
 
 
 function mostrarNaTela(){
 
    limparAll();
-   
+
    carregarTexto();
 
    letterCount();
+
    wordCount();
 
-   for (let letter in letterCounts) { 
-      const span = document.createElement("span");
-      span.className = "span__containerContador"; 
-      const textContent = `"${letter}": ${letterCounts[letter]}`;
-      span.innerText = textContent; 
+   if (letterCount){
+      for (let letter in letterCounts) {
+         const span = document.createElement("span");
+         span.className = "span__containerContador";
+         const textContent = `"${letter}": ${letterCounts[letter]}`;
+         span.innerText = textContent;
 
-      const letters = document.getElementById("lettersDiv");
-      letters.style.height = ((Object.keys(letterCounts).length * 20) / 2 ) + "px";
-      letters.appendChild(span); 
+         const letters = document.getElementById("lettersDiv");
+         letters.style.height = ((Object.keys(letterCounts).length * 20) / 2 ) + "px";
+         letters.appendChild(span);
+      }
    }
 
-   for ( let word in wordsCounts){
-      const spanWord = document.createElement("span");
-      spanWord.className = "span__containerContador";
-      const textContentWord = `"${word}": ${wordsCounts[word]}`;
-      spanWord.innerText = textContentWord;
+   if (Object.keys(wordsCounts).length > 1){
+      for ( let word in wordsCounts){
+         const spanWord = document.createElement("span");
+         spanWord.className = "span__containerContador";
+         const textContentWord = `"${word}": ${wordsCounts[word]}`;
+         spanWord.innerText = textContentWord;
 
-      const words = document.getElementById("wordsDiv");
-      words.style.height = ((Object.keys(wordsCounts).length * 20) / 2 ) + "px";
-      words.appendChild(spanWord);
+         const words = document.getElementById("wordsDiv");
+         words.style.height = ((Object.keys(wordsCounts).length * 20) / 2 ) + "px";
+         words.appendChild(spanWord);
+      }
    }
 }
